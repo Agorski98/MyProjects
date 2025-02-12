ddf03ff0982d6e1d8c8313ce882174496d54a246
Agorski98 <githubGadam@gmail.com>
2025-02-12 23:18:18 +0100

    add variable into function

--- Python_ETL/IncrementalLoad/IncrementalLoad.py
+++ Python_ETL/IncrementalLoad/IncrementalLoad.py
@@ -1,22 +1,29 @@
 
 
 ###################################################### incremental insert data
+from ast import Str
 from heapq import merge
 from sqlite3 import connect
+import string
 from tokenize import String
 from turtle import title
 import pandas as pd
-from sqlalchemy import create_engine, Table, Column, Integer, String, MetaData
+from sqlalchemy import create_engine, Table, Column, Integer, String, MetaData, literal
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.util.typing import Literal
 
 
 def configurationScript():
-    flat_filePath="B:/1_Praca/Git/MyProjects/python_ETL/IncrementalLoad/LabData.csv"
-    connection_string = "mssql+pyodbc://:AG\Asus:@AG\AG_DWH/Sample?driver=ODBC+Driver+17+for+SQL+Server"
-    select_data = "SELECT Title FROM [hol].[Lab]"
-    return flat_filePath, connection_string,select_data
 
-def flat_file_source(location):
+    engine = create_engine("mssql+pyodbc://:AG\Asus:@AG\AG_DWH/Sample?driver=ODBC+Driver+17+for+SQL+Server") #create_engine
+    query_configuration = "SELECT file_path,query,destination_table_name,table_schema FROM hol.configuration WHERE task_number=1"
+    
+    configuration_table = pd.read_sql(query_configuration,engine)
+
+    return configuration_table['file_path'][0],configuration_table['query'][0],configuration_table['destination_table_name'][0],configuration_table['table_schema'][0],engine
+
+def flat_file_source(location ):
+    
     return pd.read_csv(location,names=['Title' ,'Description', 'CategoryID','Level','Status','PublishedDate'],skiprows=[0])
     
 def select_query(query,engine):
@@ -28,12 +35,14 @@ def lookup_function(source_file_data,destination_db_data):
     no_match_data = marge_data[marge_data['_merge']=='right_only']
     return match_data, no_match_data
 
-def destination_source(engine,type_off_query,data):
+def destination_source(engine,type_off_query,data,table_schema,destination_table_name):
     metadata = MetaData()
-    metadata.reflect(bind=engine,schema='hol',only=['Lab'])
-   
-    #  print(metadata.tables.keys())  #<-show avaible tables
-    my_tab = metadata.tables['hol.Lab']
+
+    
+    metadata.reflect(bind=engine,schema=table_schema, only=[destination_table_name])
+    table_name_and_schema = table_schema + '.'+ destination_table_name
+   # print(metadata.tables.keys())  #<-show avaible tables
+    my_tab = metadata.tables[table_name_and_schema]
     type_off_query(engine,data,my_tab)
 
 def insert_row(engine,no_match_data,my_tab):   
@@ -78,19 +87,19 @@ def update_row(engine,match_data,my_tab):
    
 
 def main():
-   flat_filePath, connection_string,select_data = configurationScript() #configuration 
+   flat_filePath, select_data,destination_table_name,table_schema,engine = configurationScript() #configuration 
 
    csv_data = flat_file_source(flat_filePath) #import csv data
    
-   engine = create_engine(connection_string) #create_engine
+   
    
    database_data = select_query(select_data,engine) #import database data
    
    match_data, no_match_data = lookup_function(csv_data,database_data) #lookup function to return two dataFrame with match and no match data
    
-   destination_source(engine,insert_row,match_data) #insert new data
+   destination_source(engine,insert_row,match_data,table_schema,destination_table_name) #insert new data
    
-   destination_source(engine,update_row,match_data) #update old data
+   destination_source(engine,update_row,match_data,table_schema,destination_table_name) #update old data
 
 main()
 
