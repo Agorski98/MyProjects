56178ba32b958109e6928346aa64dea1f8dfe45e
Agorski98 <githubGadam@gmail.com>
2025-02-15 11:22:36 +0100

    Inner join etl python

---
+++ Python_ETL/InnerJoin/InnerJoin.py
@@ -0,0 +1,19 @@
+
+
+###################################################### inner join
+from ast import Str
+from heapq import merge
+from sqlite3 import connect
+import string
+from tokenize import String
+from turtle import title
+import pandas as pd
+from sqlalchemy import create_engine, Table, Column, Integer, String, MetaData, literal
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.util.typing import Literal
+
+
+
+
+
+
