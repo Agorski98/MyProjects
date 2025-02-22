a52661d4d0c5268dcf3ed20fa71d8e9a45f1bbbf
Agorski98 <githubGadam@gmail.com>
2025-02-22 20:53:23 +0100

    delete library

--- Python_ETL/IncrementalLoad/IncrementalLoad.py
+++ Python_ETL/IncrementalLoad/IncrementalLoad.py
@@ -1,12 +1,7 @@
 
 
 ###################################################### incremental insert data
-from ast import Str
-from heapq import merge
-from sqlite3 import connect
-import string
-from tokenize import String
-from turtle import title
+
 import pandas as pd
 from sqlalchemy import create_engine, Table, Column, Integer, String, MetaData, literal
 from sqlalchemy.orm import sessionmaker
--- Python_ETL/InnerJoin/InnerJoin.py
+++ Python_ETL/InnerJoin/InnerJoin.py
@@ -1,12 +1,7 @@
 
 
 ###################################################### inner join
-from ast import Str
-from heapq import merge
-from sqlite3 import connect
-import string
-from tokenize import String
-from turtle import title
+
 import pandas as pd
 from sqlalchemy import create_engine, Table, Column, Integer, String, MetaData, literal
 from sqlalchemy.orm import query, sessionmaker
@@ -50,6 +45,7 @@ def update_common_data(engine,match_data,table_schema,destination_table_name):
             session.execute(update)
         session.commit()
     except:
+
         print("Update incorect")
     finally:
         session.close()
