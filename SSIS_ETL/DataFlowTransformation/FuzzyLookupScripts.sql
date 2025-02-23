cb80eefe07a8f381a5c42d5f98ada7c719f82f87
Agorski98 <githubGadam@gmail.com>
2025-02-23 14:57:36 +0100

    Script for task

--- SSIS_ETL/DataFlowTransformation/FuzzyLookupScripts.sql
+++ SSIS_ETL/DataFlowTransformation/FuzzyLookupScripts.sql
@@ -8,13 +8,13 @@ SET NOCOUNT ON
 CREATE TABLE FuzzyLookupReference (
 FirstName NVARCHAR(50), LastName NVARCHAR(50)
 ) 
-INSERT INTO dbo.FuzzyLookupReference VALUES ( N'Syed', N'Abbas') 
-INSERT INTO dbo.FuzzyLookupReference VALUES ( N'Catherine', N'Abel') 
-INSERT INTO dbo.FuzzyLookupReference VALUES ( N'Kim', N'Abercrombie') 
-INSERT INTO dbo.FuzzyLookupReference VALUES ( N'Amy', N'Aberts') 
-INSERT INTO dbo.FuzzyLookupReference VALUES ( N'Hazem', N'Abolrous') 
-INSERT INTO dbo.FuzzyLookupReference VALUES ( N'Gustavo', N'Achong') 
-INSERT INTO dbo.FuzzyLookupReference VALUES ( N'Aaron', N'Adams') 
-INSERT INTO dbo.FuzzyLookupReference VALUES ( N'Humberto', N'Acevedo') 
-INSERT INTO dbo.FuzzyLookupReference VALUES ( N'Ronald', N'Adina') 
-INSERT INTO dbo.FuzzyLookupReference VALUES ( N'Samuel', N'Agcaoili')
+INSERT INTO dbo.FuzzyLookupReference VALUES ( N'Syed', N'Abbas'), 
+( N'Catherine', N'Abel'), 
+( N'Kim', N'Abercrombie'), 
+( N'Amy', N'Aberts'),
+( N'Hazem', N'Abolrous'), 
+( N'Gustavo', N'Achong'), 
+( N'Aaron', N'Adams'), 
+( N'Humberto', N'Acevedo'), 
+( N'Ronald', N'Adina'), 
+( N'Samuel', N'Agcaoili');
