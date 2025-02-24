430d07a16b3a86db7be93ff3493727cfe922633a
Agorski98 <githubGadam@gmail.com>
2025-02-24 21:34:59 +0100

    Data encryption in sql server

--- SQL_Security/EncryptionAndHelpfulQuery.sql
+++ SQL_Security/EncryptionAndHelpfulQuery.sql
@@ -44,7 +44,7 @@ IF EXISTS (SELECT 1 FROM dbo.Login WHERE Password = HASHBYTES('SHA2_512',@InputP
 	END
 ELSE
 	BEGIN 
-		PRINT ('Incorrect password')
+		PRINT('Incorrect password')
 	END
 
 
@@ -67,18 +67,126 @@ IF EXISTS (SELECT 1 FROM dbo.Login WHERE Password = HASHBYTES('SHA2_512',CONCAT(
 	END
 ELSE
 	BEGIN 
-		PRINT ('Incorrect password')
+		PRINT('Incorrect password')
 	END
 
 
------------------
+----------------- Encrypting data in SQL Server
+--Hierarchy of database encryption 
+-- Database Master key
+	-- Asymmetric key
+		-- Symmetric key
+
+
+SELECT name,key_algorithm, key_length
+from sys.symmetric_keys --check created key
+--WHERE name  = '##MS_DatabaseMasterKey##'
+
+
+-- Create first level encryption for Lab database
+
+
+CREATE MASTER KEY
+ENCRYPTION BY PASSWORD = '+dwDmifeIWDni12#@';
+GO
+OPEN MASTER KEY
+DECRYPTION BY PASSWORD = '+dwDmifeIWDni12#@';
+
+
+-- Remove DMK
+
+--ALTER MASTER KEY 
+--DROP ENCRYPTION BY SERVICE MASTER KEY 
+--GO
+--DROP MASTER KEY
 
 
 
+SELECT name, is_master_key_encrypted_by_server
+from sys.databases --show which database is encrypted by DMK
+
+
+BACKUP MASTER KEY
+TO FILE = 'F:\Lab_MasterKey'
+	ENCRYPTION  BY PASSWORD = '12DAsffhd*&dsa';
+GO
+
+BACKUP SERVICE MASTER KEY
+TO FILE = 'F:\Lab_SMK'
+	ENCRYPTION BY PASSWORD = '1235423FSD23^$#';
+GO
 
 
 
+--CREATE ASSYMETRIC KEY RSA
+CREATE ASYMMETRIC KEY LabASK
+WITH ALGORITHM = RSA_2048
+ENCRYPTION BY PASSWORD = '123tsdf325dfw34'
+-- RSA_4096, RSA_3072, RSA_2048
+
+
+-- CREATE CERTIFICATE (x.509)
+
+CREATE CERTIFICATE LabCERT
+ENCRYPTION BY PASSWORD = 'dsa123t132sdf325dfw34'
+WITH SUBJECT = 'LAB Certyfikate',
+EXPIRY_DATE = '20251231' 
+
+
+CREATE SYMMETRIC KEY LabSMK
+WITH ALGORITHM = AES_256
+ENCRYPTION BY CERTIFICATE LabCERT 
+
+ALTER DATABASE Lab
+SET COMPATIBILITY_LEVEL = 160
+
+CREATE ASYMMETRIC KEY testASK
+WITH ALGORITHM = RSA_512 --not works
+
+
+--ALTER DATABASE Lab
+--SET COMPATIBILITY_LEVEL = 120
+
+
+--CREATE ASYMMETRIC KEY testASK
+--WITH ALGORITHM = RSA_512 --now its works but this key is deprecated today
+
+
+-- If you encrypted data with old algorithm you must depcrypt and encrypt data again using another supported key
+-- But is one exception
+
+
+CREATE SYMMETRIC KEY LabSMK1
+WITH ALGORITHM = AES_256
+ENCRYPTION BY ASYMMETRIC KEY LabASK 
+
+OPEN SYMMETRIC KEY LabSMK1
+DECRYPTION BY ASYMMETRIC KEY  LabASK
+WITH PASSWORD = '123tsdf325dfw34'
+
+
+--CREATE NEW ASYMMETRIC KEY
+
+CREATE ASYMMETRIC KEY NEW_KeyASK
+WITH ALGORITHM = RSA_4096 
+ENCRYPTION BY PASSWORD = '132fdsd2d34'
 
 
 
+ALTER SYMMETRIC KEY LabSMK1
+ADD ENCRYPTION BY ASYMMETRIC KEY NEW_KeyASK
 
+ALTER SYMMETRIC KEY LabSMK1
+DROP ENCRYPTION BY ASYMMETRIC KEY LabASK
+
+
+CLOSE SYMMETRIC KEY LabSMK1
+
+
+OPEN SYMMETRIC KEY LabSMK1
+DECRYPTION BY ASYMMETRIC KEY  NEW_KeyASK
+WITH PASSWORD = '132fdsd2d34'
+
+
+
+------------------------ column encryption
