2b2da74d5c2e4f2076fd0a8a1dc6637e38725c36
Agorski98 <githubGadam@gmail.com>
2025-02-24 19:19:58 +0100

    Changing Code format

--- SQL_Security/EncryptionAndHelpfulQuery.sql
+++ SQL_Security/EncryptionAndHelpfulQuery.sql
@@ -1,13 +1,16 @@
 
 
 --SELECT *  FROM sys.dm_exec_connections -- This check whether enable encryption works correct
+----------- Hash function
+
 
 USE Lab
-
+GO
+DROP TABLE IF EXISTS dbo.Login
 Create table dbo.Login (
-LoginId int Identity (1,1) PRIMARY KEY,
-UserName NVARCHAR(50) NOT NULL UNIQUE,
-Password VARBINARY(128) NOT NULL
+	LoginId int Identity (1,1) PRIMARY KEY,
+	UserName NVARCHAR(50) NOT NULL UNIQUE,
+	Password VARBINARY(128) NOT NULL
 )
 GO
 
@@ -19,10 +22,10 @@ DECLARE @HashedPassword VARBINARY (256)
 
 SET @HashedPassword = HASHBYTES('SHA2_512', @Password) --Hasbytes is determistic function two equal string must return the same result
 -- Hashes is one-way irreversible algorithms 
--- HASHBYTES is only one hash function but he not do add salt
+-- HASHBYTES is only one hash function in tsql but we not can add salt 
 
 
-SELECT DATALENGTH (@HashedPassword)
+--SELECT DATALENGTH (@HashedPassword)
 
 INSERT INTO dbo.Login (UserName,Password)
 VALUES (N'DrWho',HASHBYTES('SHA2_512',N'Cyberm3n!'))
@@ -35,20 +38,19 @@ DECLARE @InputPassword Nvarchar(50) = N'Cyberm3n!'
 DECLARE @user Nvarchar(50) = N'DrWho'
 
 
-IF EXISTS (SELECT 1 FROM dbo.Login WHERE
-Password = HASHBYTES('SHA2_512',@InputPassword) AND UserName = @user)
-BEGIN 
-PRINT('Correct password')
-END
+IF EXISTS (SELECT 1 FROM dbo.Login WHERE Password = HASHBYTES('SHA2_512',@InputPassword) AND UserName = @user)
+	BEGIN 
+		PRINT('Correct password')
+	END
 ELSE
-BEGIN 
-PRINT ('Incorrect password')
-END
+	BEGIN 
+		PRINT ('Incorrect password')
+	END
 
 
-
+---------------------------- HASH with salt
 -- a good rules is salt password 
--- a salt password is a password with a random string. This action increases the length of the password and makes it more difficult to crack.
+-- a salt password is a password with add a random string. This action increases the length of the password and makes it more difficult to crack.
 
 DECLARE @InputPassword Nvarchar(50) = N'Another!2'
 DECLARE @user Nvarchar(50) = N'Edek'
@@ -59,18 +61,17 @@ DECLARE @user Nvarchar(50) = N'Edek'
 
 
 
-IF EXISTS (SELECT 1 FROM dbo.Login WHERE
-Password = HASHBYTES('SHA2_512',CONCAT(@InputPassword,@user)) AND UserName = @user)
-BEGIN 
-PRINT('Correct password')
-END
+IF EXISTS (SELECT 1 FROM dbo.Login WHERE Password = HASHBYTES('SHA2_512',CONCAT(@InputPassword,@user)) AND UserName = @user)
+	BEGIN 
+		PRINT('Correct password')
+	END
 ELSE
-BEGIN 
-PRINT ('Incorrect password')
-END
+	BEGIN 
+		PRINT ('Incorrect password')
+	END
 
 
-
+-----------------
 
 
 
